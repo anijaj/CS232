e49543f817f6ea91bdeb66fdc203cf0c3044738c
Anija J <44459158+ceccs17d07@users.noreply.github.com>
2019-05-09 17:55:24 +0100

    Create [Task 5] Transfering_sftp.sh

---
+++ [Task 5] Transfering_sftp.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+touch sftp_test_1.txt
+sftp cs17d07@http://14.139.189.217
+?
+version
+ls
+pwd
+lpwd
+mkdir sftp_test
+cd sftp_test
+put sftp_test_1.txt
+rename sftp_test_1.txt sftp_t1.txt
+lcd /home/georgi/Desktop
+get sftp_t1.txt
+rm sftp_t1.txt
+cd ..
+rmdir test
+exit
